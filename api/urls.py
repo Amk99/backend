999268f2858ca8601ac4a57adb356312edcd6d47
atharva kotibhaskar <atharvatamk@gmail.com>
2022-11-25 15:03:34 +0530

    added Logout function

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
     path('login', views.login),
+    path('logout', views.Logout.as_view()),
     path('follow/<int:pk>/', views.FollowView.as_view({'post': 'follow'})),
     path('unfollow/<int:pk>/', views.FollowView.as_view({'post': 'unfollow'})),
 ]
--- api/views.py
+++ api/views.py
@@ -7,7 +7,7 @@ from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny,IsAuthenticated
 from rest_framework.response import Response
-
+from rest_framework.views import APIView
 from django.shortcuts import render,redirect
 from django.http import HttpResponseRedirect
 from django.urls import reverse
@@ -47,6 +47,15 @@ def login(request):
     token, _ = Token.objects.get_or_create(user=user)
     return Response({'token': token.key},
                     status=HTTP_200_OK)
+
+
+@permission_classes((AllowAny,))
+class Logout(APIView):
+    def get(self, request, format=None):
+        # simply delete the token to force a login
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
+
 
 
 class FollowView(viewsets.ViewSet):
