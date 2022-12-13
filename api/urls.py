1b008e16ee80f2d8c062c38793d947769c9d2a12
atharva kotibhaskar <atharvatamk@gmail.com>
2022-12-13 14:40:55 +0530

    added post creation and viewing

--- api/urls.py
+++ api/urls.py
@@ -6,10 +6,13 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
-    path('login', views.login),
-    path('logout', views.Logout.as_view()),
+    path('login/', views.AuthClass.as_view()),
+    path('logout/', views.Logout.as_view()),
+    path('post',views.PostCreateView.as_view()),
     path('follow/<int:pk>/', views.FollowView.as_view({'post': 'follow'})),
     path('unfollow/<int:pk>/', views.FollowView.as_view({'post': 'unfollow'})),
+    path('post/<int:pk>/', views.PostDetail.as_view()),
+
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
