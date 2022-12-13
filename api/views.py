8f26472e009301e301a9b85e47b4a878ec6acf6c
atharva kotibhaskar <atharvatamk@gmail.com>
2022-12-13 14:43:07 +0530

    post creation and changed function based to class based

--- api/views.py
+++ api/views.py
@@ -2,15 +2,11 @@ from rest_framework import generics,viewsets,status
 from api import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework.authtoken.models import Token
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny,IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from django.shortcuts import render,redirect
-from django.http import HttpResponseRedirect
-from django.urls import reverse
+from .models import Post
 
 from rest_framework.status import (
     HTTP_400_BAD_REQUEST,
@@ -27,34 +23,35 @@ class UserList(generics.ListAPIView):
     serializer_class = serializers.UserSerializer
 
 class UserDetail(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
 
 
-@csrf_exempt
-@api_view(["POST"])
-@permission_classes((AllowAny,))
-def login(request):
-    email = request.data.get("email")
-    password = request.data.get("password")
-    if email is None or password is None:
-        return Response({'error': 'Please provide both email and password'},
-                        status=HTTP_400_BAD_REQUEST)
-    user = authenticate(email=email, password=password)
-    if not user:
-        return Response({'error': 'Invalid Credentials'},
-                        status=HTTP_404_NOT_FOUND)
-    token, _ = Token.objects.get_or_create(user=user)
-    return Response({'token': token.key},
-                    status=HTTP_200_OK)
+class AuthClass(APIView):
+    http_method_names = ['post']
+    permission_classes = (AllowAny,)
+    def post(self, request,format = None):
+        email = request.data.get("email")
+        password = request.data.get("password")
+        if email is None or password is None:
+            return Response({'error': 'Please provide both email and password'},
+                            status=HTTP_400_BAD_REQUEST)
+        user = authenticate(email=email, password=password)
+        if not user:
+            return Response({'error': 'Invalid Credentials'},
+                            status=HTTP_404_NOT_FOUND)
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key},
+                        status=HTTP_200_OK)
 
-
-@permission_classes((AllowAny,))
 class Logout(APIView):
+    permission_classes = [IsAuthenticated]
     def get(self, request, format=None):
         # simply delete the token to force a login
         request.user.auth_token.delete()
-        return Response(status=status.HTTP_200_OK)
+        return Response({'You have been logged out'},status=status.HTTP_200_OK)
+
 
 
 
@@ -71,6 +68,18 @@ class FollowView(viewsets.ViewSet):
         profile = User.objects.get(pk=pk)
         profile.followers.remove(request.user)
         return Response({'message': 'you have unfollowed this user'}, status=status.HTTP_200_OK)
-        # your unfollow code
 
-# change views https://www.appsloveworld.com/django/100/101/best-implementation-follow-and-unfollow-in-django-rest-framework
+
+class PostCreateView(generics.CreateAPIView):
+    permission_classes = [IsAuthenticated]
+    model = Post
+    serializer_class = serializers.PostSerialzer
+    def perform_create(self, serializer):
+        serializer.save(creator_id=self.request.user.id)
+
+
+
+class PostDetail(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerialzer
